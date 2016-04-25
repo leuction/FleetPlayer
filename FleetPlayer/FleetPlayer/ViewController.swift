a703bc5ab1e65fe9dd5d513a6a6023b42601b4d7
leuction <unconfigured@null.spigotmc.org>
2016-04-25 18:35:39 +0800

    init commit

---
+++ FleetPlayer/FleetPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FleetPlayer
+//
+//  Created by 硕 王 on 4/25/16.
+//  Copyright © 2016 硕 王. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
